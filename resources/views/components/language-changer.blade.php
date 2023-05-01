71343d13347263a359f72a79aa837d6485535277
Leonardo <leo.abreudp@gmail.com>
2023-05-01 21:02:44 +0100

    languages in the sidebar

--- resources/views/components/language-changer.blade.php
+++ resources/views/components/language-changer.blade.php
@@ -1,4 +1,14 @@
 <div class='grid grid-cols-2 gap-1'>
-  <a href="{{route('language', ['locale' => 'pt'])}}" class='py-1 px-2 @if($current == 'PT') bg-slate-700 @else bg-black @endif text-white text-xs rounded'>PT</a>
-  <a href="{{route('language', ['locale' => 'en'])}}" class='py-1 px-2 @if($current == 'EN') bg-slate-700 @else bg-black @endif text-white text-xs rounded'>EN</a>
+  <a href="{{route('language', ['locale' => 'pt'])}}" class='py-1 px-2 @if($current == 'PT') bg-slate-700 @else bg-slate-500 @endif hover:bg-slate-600 text-white text-xs rounded flex flex-row justify-between'>
+    <p class="text-xl">PT</p>
+    <div class="w-12">
+      <img src="{{asset('images/pt.jpg')}}" alt="Portugal flag">
+    </div>
+  </a>
+  <a href="{{route('language', ['locale' => 'en'])}}" class='py-1 px-2 @if($current == 'EN') bg-slate-700 @else bg-slate-500 @endif hover:bg-slate-600 text-white text-xs rounded flex flex-row justify-between'>
+    <p class="text-xl">EN</p>
+    <div class="w-12">
+      <img src="{{asset('images/uk.jpg')}}" alt="Portugal flag">
+    </div>
+  </a>
 </div>
--- resources/views/layouts/admin-layout.blade.php
+++ resources/views/layouts/admin-layout.blade.php
@@ -12,31 +12,31 @@
        <ul class="space-y-2 font-medium flex flex-col justify-between h-full">
         <div class="space-y-2">
           <li>
-              <a href="{{route('admin.list_users')}}" class="flex items-center p-2 text-white rounded-lg @if(Session::get('place') == 'admin_usr') bg-slate-600 @endif hover:bg-slate-600">
+              <a href="{{route('admin.list_users')}}" class="flex items-center p-2 text-white rounded-lg @if(Session::get('place') == 'admin_usr') bg-slate-700 @endif hover:bg-slate-600">
               <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
                 <span class="flex-1 ml-3 whitespace-nowrap">@lang('Users')</span>
               </a>
           </li>
           <li>
-              <a href="{{route('admin.list')}}" class="flex items-center p-2 text-white rounded-lg @if(Session::get('place') == 'admin_attr') bg-slate-600 @endif hover:bg-slate-600">
+              <a href="{{route('admin.list')}}" class="flex items-center p-2 text-white rounded-lg @if(Session::get('place') == 'admin_attr') bg-slate-700 @endif hover:bg-slate-600">
               <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path d="M3 3h18v18H3zM21 9H3M21 15H3M12 3v18"/></svg>
                 <span class="flex-1 ml-3 whitespace-nowrap">@lang('Attractions')</span>
               </a>
           </li>
         </div>
-        <div>
+        <div class="space-y-2">
           <li>
-              <a href="{{route('signout')}}" class="flex items-center p-2 text-white rounded-lg hover:bg-slate-600">
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4M10 17l5-5-5-5M13.8 12H3"/></svg>
-                <span class="flex-1 ml-3 whitespace-nowrap">@lang('Sign Out')</span>
-              </a>
+            <x-language-changer :current="$current"/>
+          </li>
+          <li>
+            <a href="{{route('signout')}}" class="flex items-center p-2 text-white rounded-lg hover:bg-slate-600">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4M10 17l5-5-5-5M13.8 12H3"/></svg>
+              <span class="flex-1 ml-3 whitespace-nowrap">@lang('Sign Out')</span>
+            </a>
           </li>
         </div>
        </ul>
     </div>
-  </div>
-  <div class='absolute top-1 right-0 p-4'>
-    <x-language-changer :current="$current"/>
   </div>
   <div class="pl-64">
     @yield('body')
