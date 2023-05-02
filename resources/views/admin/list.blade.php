dd70003c89143ad2fe5f0eb79228f0feaa6ac571
Leonardo <leo.abreudp@gmail.com>
2023-05-02 22:26:00 +0100

    no attractions list warning fixed

--- resources/views/admin/list.blade.php
+++ resources/views/admin/list.blade.php
@@ -7,26 +7,26 @@
         <x-a :url="route('admin.create')" :name="'Create Attraction'"/>
       </div>
     </div>
-    <div class='grid grid-cols-6 p-4 border-2 rounded-t border-black border-b-0 w-full'>
-      <h1 class="text-lg">@lang('Qr Code')</h1>
-      <h1 class='text-lg'>@lang('Name')</h1>
-      <h1 class="text-lg">@lang('Description')</h1>
-      <h1 class="text-lg">@lang('Created by'):</h1>
-      <h1 class="text-lg">@lang('Created at'):</h1>
-      <h1 class="text-lg">@lang('Actions')</h1>
-    </div>
-    <div class="w-full">
-      @if(count($attractions) != 0)
+    @if(count($attractions) != 0)
+      <div class='grid grid-cols-6 p-4 border-2 rounded-t border-black border-b-0 w-full'>
+        <h1 class="text-lg">@lang('Qr Code')</h1>
+        <h1 class='text-lg'>@lang('Name')</h1>
+        <h1 class="text-lg">@lang('Description')</h1>
+        <h1 class="text-lg">@lang('Created by'):</h1>
+        <h1 class="text-lg">@lang('Created at'):</h1>
+        <h1 class="text-lg">@lang('Actions')</h1>
+      </div>
+    @else
+      <div class="w-full">
         @foreach ($attractions as $attr)
           <div class="@if(!$loop->last) border-b-0 @else rounded-b @endif border-2 border-black">
             <x-attraction :attraction="$attr"/>
           </div>
         @endforeach
-      @else
-        <div class="p-4">
-          <h1 class='text-2xl'>@lang('There are no attractions created')</h1>
-        </div>
-      @endif
-    </div>
+      </div>
+      <div class="p-4">
+        <h1 class='text-2xl'>@lang('There are no attractions created')</h1>
+      </div>
+    @endif
   </div>
 @endsection
