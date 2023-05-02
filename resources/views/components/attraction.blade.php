409b1079a8671b2823f53bb3f22930713b98c5a1
Leonardo <leo.abreudp@gmail.com>
2023-05-02 21:29:50 +0100

    qr code download correctly

--- resources/views/components/attraction.blade.php
+++ resources/views/components/attraction.blade.php
@@ -1,7 +1,7 @@
 <div class='grid grid-cols-6 p-4 w-full h-36'>
   <x-delete-alert :route="route('admin.delete', ['id' => $attraction->id])" :id="$attraction->id"/>
   <div class="h-28 w-28 flex flex-row items-center">
-    <a target="_blank" href="<?=route('view', ['title_compiled' => $attraction->title_compiled])?>?print=true"><img src="{{ asset($attraction['qr-code']) }}" alt="Local Image" class='w-full h-full'></a>
+    <a target="_blank" href="{{asset($attraction['qr-code'])}}" download="{{$attraction->title}}"><img src="{{ asset($attraction['qr-code']) }}" alt="Local Image" class='w-full h-full'></a>
   </div>
   <h1 class='flex flex-row items-center text-xl'>{{$attraction->title}}</h1>
   <h1 class='flex flex-row items-center text-xl max-h-28'>{{$attraction->description}}</h1>
