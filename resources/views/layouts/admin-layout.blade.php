eeb8bce64488b15bf288eb5342d50636275d51ad
Leonardo <leo.abreudp@gmail.com>
2023-05-02 16:53:48 +0100

    sidebar order

--- resources/views/layouts/admin-layout.blade.php
+++ resources/views/layouts/admin-layout.blade.php
@@ -12,15 +12,15 @@
        <ul class="space-y-2 font-medium flex flex-col justify-between h-full">
         <div class="space-y-2">
           <li>
-              <a href="{{route('admin.list_users')}}" class="flex items-center p-2 text-white rounded-lg @if(Session::get('place') == 'admin_usr') bg-slate-700 @endif hover:bg-slate-600">
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
-                <span class="flex-1 ml-3 whitespace-nowrap">@lang('Users')</span>
-              </a>
-          </li>
-          <li>
               <a href="{{route('admin.list')}}" class="flex items-center p-2 text-white rounded-lg @if(Session::get('place') == 'admin_attr') bg-slate-700 @endif hover:bg-slate-600">
               <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path d="M3 3h18v18H3zM21 9H3M21 15H3M12 3v18"/></svg>
                 <span class="flex-1 ml-3 whitespace-nowrap">@lang('Attractions')</span>
+              </a>
+          </li>
+          <li>
+              <a href="{{route('admin.list_users')}}" class="flex items-center p-2 text-white rounded-lg @if(Session::get('place') == 'admin_usr') bg-slate-700 @endif hover:bg-slate-600">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
+                <span class="flex-1 ml-3 whitespace-nowrap">@lang('Users')</span>
               </a>
           </li>
         </div>
