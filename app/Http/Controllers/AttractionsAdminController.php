6802c1f2aace57671980e09e8782f8606f97d2de
DaniloKy <danilokymhyr@gmail.com>
2023-05-02 21:07:38 +0100

    merge currect

--- app/Http/Controllers/AttractionsAdminController.php
+++ app/Http/Controllers/AttractionsAdminController.php
@@ -30,15 +30,9 @@ class AttractionsAdminController extends Controller
   public function create(Request $request)
   {
     $validatedData = $request->validate([
-      'title' => 'required|unique:attractions,title',
-      'description' => 'required'
+        'title' => 'required|unique:attractions,title',
+        'description' => 'required'
     ]);
-    public function create(Request $request)
-    {
-        $validatedData = $request->validate([
-            'title' => 'required|unique:attractions,title',
-            'description' => 'required'
-        ]);
 
     $title = $validatedData['title'];
 
