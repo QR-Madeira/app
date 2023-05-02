9cf55eb0a9193ed10fae8fecd204469e67c4edd0
Leonardo <leo.abreudp@gmail.com>
2023-05-02 22:11:35 +0100

    gallery and map controller and view

--- app/Http/Controllers/AttractionsViewerController.php
+++ app/Http/Controllers/AttractionsViewerController.php
@@ -20,4 +20,32 @@ class AttractionsViewerController extends Controller
 
     return $this->view('viewer.get');
   }
+
+  public function gallery($title_compiled)
+  {
+    $attraction = Attraction::where('title_compiled', '=', $title_compiled)->first()->toArray();
+    $description = nl2br($attraction['description']);
+
+    $this->data->set('image', 'storage/attractions/' . $attraction['image_path']);
+    $this->data->set('title_compiled', $title_compiled);
+    $this->data->set('title', $attraction['title']);
+    $this->data->set('description', $description);
+    $this->data->set('qr', asset('storage/qr-codes/' .  $attraction["qr-code_path"]));
+
+    return $this->view('viewer.gallery');
+  }
+
+  public function map($title_compiled)
+  {
+    $attraction = Attraction::where('title_compiled', '=', $title_compiled)->first()->toArray();
+    $description = nl2br($attraction['description']);
+
+    $this->data->set('image', 'storage/attractions/' . $attraction['image_path']);
+    $this->data->set('title_compiled', $title_compiled);
+    $this->data->set('title', $attraction['title']);
+    $this->data->set('description', $description);
+    $this->data->set('qr', asset('storage/qr-codes/' .  $attraction["qr-code_path"]));
+
+    return $this->view('viewer.map');
+  }
 }
