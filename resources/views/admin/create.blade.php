a0917cded2642e7d4be6e73f620da67c08cd77b2
DaniloKy <danilokymhyr@gmail.com>
2023-05-03 17:35:06 +0100

    working on close locations

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -12,6 +12,16 @@
       <x-input :type="'text'" :name="'title'" :placeholder="'Title'"/>
       <textarea type="text" name="description" placeholder="@lang('Description')" class="p-4 bg-black/[.10] text-black rounded-lg placeholder:text-black"></textarea>
       <x-input :type="'file'" :name="'image'" :placeholder="'Image'"/>
+      <h2 class='text-center text-3xl py-8'>@lang('Close locations')</h2>
+      <label for="close_icon">Chose an icon: </label>
+      <select name="close_icon" id="close_icon">
+        <option data-icon="glyphicon-glass" value="">A</option>
+        <option data-icon="glyphicon-glass" value="">B</option>
+        <option data-icon="glyphicon-glass" value="">C</option>
+      </select>
+      <x-input :type="'text'" :name="'close_name'" :placeholder="'Place name'"/>
+      <x-input :type="'text'" :name="'close_location'" :placeholder="'Place location'"/>
+      <x-input :type="'text'" :name="'close_phone'" :placeholder="'Place phone'"/>
       <x-submit :value="'Create'"/>
     </form>
     @if($created)
