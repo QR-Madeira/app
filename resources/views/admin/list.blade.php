189c5b1d26741d19457c895baac702f7f10e2d61
Leonardo <leo.abreudp@gmail.com>
2023-05-12 12:33:31 +0100

    just missing list gallery

--- resources/views/admin/list.blade.php
+++ resources/views/admin/list.blade.php
@@ -1,17 +1,18 @@
 @extends('layouts.admin-layout')
 @section('body')
-  <div class='px-24 py-4 grid grid-rows-auto grid-cols-1 justify-items-center'>
-    <div class='flex items-center justify-center w-full relative'>
-      <h1 class='text-5xl py-8'>@lang('Attractions List')</h1>
-      <div class='absolute right-0'>
+  <div class='lg:px-24 px-2 py-4 grid grid-rows-auto grid-cols-1 justify-items-center'>
+    <div class='flex lg:flex-row flex-col items-center justify-center w-full relative pb-12'>
+      <h1 class='text-5xl text-center py-8'>@lang('Attractions List')</h1>
+      <div class='lg:absolute lg:right-0'>
         <x-a :url="route('admin.create.attraction')" :name="'Create Attraction'"/>
       </div>
     </div>
     @if(count($attractions) != 0)
-      <div class='grid grid-cols-5 p-4 border-2 rounded-t border-black border-b-0 w-full'>
+      <div class='grid lg:grid-cols-5 grid-cols-2 p-4 border-2 rounded-t border-black border-b-0 w-full'>
         <h1 class="text-lg">@lang('Qr Code')</h1>
-        <h1 class='text-lg'>@lang('Name')</h1>
-        <h1 class="text-lg">@lang('Description')</h1>
+        <h1 class='text-lg lg:block hidden'>@lang('Name')</h1>
+        <h1 class="text-lg lg:block hidden">@lang('Description')</h1>
+        <h1 class="text-lg text-end">@lang('Actions')</h1>
       </div>
       <div class="w-full">
         @foreach ($attractions as $attr)
