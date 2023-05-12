267a8ae89de76544a005d38c5e82ba3d26e512cf
Leonardo <leo.abreudp@gmail.com>
2023-05-12 12:11:38 +0100

    sidebar mobile

--- resources/views/layouts/admin-layout.blade.php
+++ resources/views/layouts/admin-layout.blade.php
@@ -4,24 +4,21 @@
   <x-header/>
 </head>
 <body>
-  <div class="fixed top-0 left-0 w-64 h-screen">
+  <div class="fixed top-0 left-0 lg:w-64 w-20 h-screen">
     <div class="h-full px-3 py-4 overflow-y-auto bg-slate-500 flex flex-col">
-      <div class="w-auto h-auto rounded-lg bg-slate-700 items-center text-left mb-4 p-2">
-        <p class="ml-2 mr-2 w-auto h-auto break-all text-white">{{Auth::user()->name}}</p>
-      </div>
        <ul class="space-y-2 font-medium flex flex-col justify-between h-full">
         <div class="space-y-2">
           <li>
-              <a href="{{route('admin.list.attraction')}}" class="flex items-center p-2 text-white rounded-lg @if(Session::get('place') == 'admin_attr') bg-slate-700 @endif hover:bg-slate-600">
-                <span class="material-symbols-rounded fs-36">distance</span>
-                <span class="flex-1 ml-3 whitespace-nowrap">@lang('Attractions')</span>
-              </a>
+            <a href="{{route('admin.list.attraction')}}" class="flex items-center p-2 text-white rounded-lg @if(Session::get('place') == 'admin_attr') bg-slate-700 @endif hover:bg-slate-600">
+              <span class="material-symbols-rounded fs-36">distance</span>
+              <span class="flex-1 ml-3 whitespace-nowrap hidden lg:block">@lang('Attractions')</span>
+            </a>
           </li>
           <li>
-              <a href="{{route('admin.list.users')}}" class="flex items-center p-2 text-white rounded-lg @if(Session::get('place') == 'admin_usr') bg-slate-700 @endif hover:bg-slate-600">
-                <span class="material-symbols-rounded fs-36">group</span>
-                <span class="flex-1 ml-3 whitespace-nowrap">@lang('Users')</span>
-              </a>
+            <a href="{{route('admin.list.users')}}" class="flex items-center p-2 text-white rounded-lg @if(Session::get('place') == 'admin_usr') bg-slate-700 @endif hover:bg-slate-600">
+              <span class="material-symbols-rounded fs-36">group</span>
+              <span class="flex-1 ml-3 whitespace-nowrap hidden lg:block">@lang('Users')</span>
+            </a>
           </li>
         </div>
         <div class="space-y-2">
@@ -31,14 +28,14 @@
           <li>
             <a href="{{route('signout')}}" class="flex items-center p-2 text-white rounded-lg hover:bg-slate-600">
               <span class="material-symbols-rounded fs-36">logout</span>
-              <span class="flex-1 ml-3 whitespace-nowrap">@lang('Sign Out')</span>
+              <span class="flex-1 ml-3 whitespace-nowrap lg:block hidden">@lang('Sign Out')</span>
             </a>
           </li>
         </div>
        </ul>
     </div>
   </div>
-  <div class="pl-64">
+  <div class="lg:pl-64 pl-20">
     @yield('body')
   </div>
 </body>
