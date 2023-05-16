6982b310284f291f455bc00c92238d75226fd6c4
Leonardo <leo.abreudp@gmail.com>
2023-05-16 17:30:56 +0100

    serve db

--- serve.sh
+++ serve.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
 if [ "$1" = "--init" ]; then
-    git pull
-    composer i && npm i
-    rm -rf public/storage
-    rm -rf storage/app/public/*
-    php artisan storage:link
-    php artisan migrate:fresh --seed
+  git pull
+  composer i && npm i
+  rm -rf public/storage
+  rm -rf storage/app/public/*
+  php artisan storage:link
+  php artisan migrate:fresh --seed
+fi
+
+if [ "$1" = "--db" ]; then
+  git pull
+  php artisan migrate:fresh --seed
 fi
 
 stop_commands() {
