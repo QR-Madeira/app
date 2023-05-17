836bc1ae0f6e50f3e49d9a6d736e9e1d1c55fef0
torres-developer <torres.dev@disroot.org>
2023-05-17 18:46:51 +0100

    i18n

--- resources/views/admin/create_close_location.blade.php
+++ resources/views/admin/create_close_location.blade.php
@@ -14,9 +14,9 @@
 <table class="my-4 border border-slate-500 w-full">
 
 <caption>
-<strong>Close Attractions List</strong>
+<strong>@lang("Close Attractions List")</strong>
 <details>
-<summary>What's a Close Attraction?</summary>
+<summary>@lang("What's a Close Attraction?")</summary>
 <p>Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.</p>
 </details>
 </caption>
@@ -78,7 +78,7 @@
 
 <fieldset>
 
-<!-- <legend>@lang("Phone")</legend> -->
+<legend>@lang("Phone")</legend>
 
 <div class="flex flex-row justify-center gap-4">
 <div class="flex flex-col">
