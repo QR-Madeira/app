b01d0adb2fc5956e5132e32b2216cf90322a0b3c
torres-developer <torres.dev@disroot.org>
2023-05-28 22:01:32 +0100

    html

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -16,7 +16,7 @@
         </ul>
     </address>
     <div class="grid sm:grid-cols-2 gap-4 [&>*]:text-gray-500">
-        <div class="flex flex-row gap-4 bg-white p-4">
+        <div class="flex flex-row gap-4 bg-white px-4">
             <div class="flex text-center">
                 <p>&copy; {{$site['footerCopyright']}}</p>
             </div>
@@ -37,7 +37,7 @@
             </div>
         </div>
             @if(!empty($site["socials"]))
-        <section class="flex flex-row gap-4 bg-white p-4">
+        <section class="flex flex-row gap-4 bg-white px-4">
             <ul class="flex max-w-[35ch] overflow-y-auto p-4 sm:m-auto gap-4 sm:justify-center">
             @foreach($site["socials"] as $s)
             <li><a target="_blank" href="{{$s["uri"]}}"><img src="/images/{{$s["ico"]}}" width="32" class="w-[32px] aspect-square object-cover" alt="{{$s["description"]}}" /></a></li>
