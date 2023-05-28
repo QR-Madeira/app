76753bd9d75158ffb0c8243ea4ce46a9a3d3d62a
torres-developer <torres.dev@disroot.org>
2023-05-28 22:29:50 +0100

    font swap

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,8 @@
 <meta charset="UTF-8">
+<meta name="description" content="QR-Madeira" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <!--<link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded" rel="stylesheet">-->
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,300,0,0" />
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,300,0,0&display=swap" />
 @vite('resources/css/app.css')
 <title>QR-Madeira</title>
